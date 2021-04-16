4ff971d34944fb5263fb92ca7e744c6ff4de6faa
Dmitry <dmitry.zaytsev83@gmail.com>
2021-04-16 17:58:46 +0300

    fix semicommas

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux'
-import {Provider} from 'react-redux'
-import rootReducer from './reducer/rootReducer'
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './reducer/rootReducer';
 
 const store = createStore(rootReducer)
 
